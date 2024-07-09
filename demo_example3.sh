ea213e48e442adfeed0c61f1977094cd5bcd582d
배창현 <chbae@gcsc.co.kr>
2024-07-09 04:19:40 +0000

    ✅ Change default device(GPU->CPU)

--- demo_example3.sh
+++ demo_example3.sh
@@ -6,9 +6,9 @@ rm results/style3_seg.pgm -rf;
 rm results/stylization_with_auto_segmentation.png -rf;
 export PYTHONPATH=$PYTHONPATH:segmentation
 cd images;
-axel -n 1 https://pre00.deviantart.net/f1a6/th/pre/i/2010/019/0/e/country_road_hdr_by_mirre89.jpg --output=content3.png;
-axel -n 1 https://inchiostro.unipv.it/wp-content/uploads/2017/10/Stranger_Things_S2_news_Images_V03-1024x481.jpg --output=style3.png;
+wget https://pre00.deviantart.net/f1a6/th/pre/i/2010/019/0/e/country_road_hdr_by_mirre89.jpg -O content3.png
+wget https://inchiostro.unipv.it/wp-content/uploads/2017/10/Stranger_Things_S2_news_Images_V03-1024x481.jpg -O style3.png
 convert -resize 50% content3.png content3.png;
 convert -resize 50% style3.png style3.png;
 cd ..;
-python demo_with_ade20k_ssn.py;
+python demo_with_ade20k_ssn.py --cuda 0;
